64018ce40c331ad32294fe178a86b4516ef72fd2
ricardojlrufino <ricardo.jl.rufino@gmail.com>
2015-02-22 16:30:45 -0300

    update examples

--- examples/UsbConnection/UsbConnection.ino
+++ examples/UsbConnection/UsbConnection.ino
@@ -1,28 +1,18 @@
 /*
  * *****************************************************************************
- * See tutorial: https://opendevice.atlassian.net/wiki/display/DOC/A.+First+Steps+with+OpenDevice
+ * See tutorial: https://opendevice.atlassian.net/wiki/display/DOC/A.+First+Steps+with+ODev
  * This example code is in the public domain.
  * *****************************************************************************
  */
-
-#include <DeviceConnection.h>
 #include <OpenDevice.h>
 
 void setup(){
-    OpenDevice.debugMode = true;
-    Serial.begin(9600);  
-    while (!Serial){delay(1);} // uncomment if using Leonardo
-   
-    OpenDevice.addDevice(13, Device::DIGITAL); // ID:1
-    
-    OpenDevice.begin(Serial);
-    OpenDevice.debug("Started!");
-         
+    ODev.enableDebug();
+    ODev.addDevice(13, Device::DIGITAL); // ID:1
+    ODev.begin(); // by default call Serial.begin() and while(!Serial) on Leonardo
 }
 
 void loop(){
-	
-	OpenDevice.loop();
-
+	ODev.loop();
 }
 
