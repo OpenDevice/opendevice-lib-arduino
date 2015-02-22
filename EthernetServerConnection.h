03902eaaa07db16b78415ad5a41b49f002093b33
ricardojlrufino <ricardo.jl.rufino@gmail.com>
2015-02-22 17:54:08 -0300

    ethernet class cleanup

--- EthernetServerConnection.h
+++ EthernetServerConnection.h
@@ -15,10 +15,10 @@
  * EthernetServerConnection.h
  *
  *  Created on: 19/02/2015
- *      Author: ricardo
+ *      Author: Ricardo JL Rufino
  */
 #include <Arduino.h>
-#include "config.h"
+#include <Stream.h>
 
 #if defined (UIPETHERNET_H)
 	#include <UIPEthernet.h>
@@ -26,13 +26,13 @@
 	#include <Ethernet.h>
 #endif
 
-#include <Stream.h>
+#include "config.h"
 #include "DeviceConnection.h"
 
 #define USING_CUSTOM_CONNECTION 1
 #define CUSTOM_CONNECTION_CLASS EthernetClient
 
-EthernetServer server(80);
+EthernetServer ethserver(80);
 EthernetClient ethclient;
 IPAddress myIP(192, 168, 2, 106);
 
@@ -47,88 +47,18 @@ void custom_connection_begin(){
 		Ethernet.begin(mac, myIP);
 //	}
 	Serial.print("Server is at: "); Serial.println(Ethernet.localIP());
-	server.begin();
+	ethserver.begin();
 
 }
 
 /** This method is called automatically by the OpenDevice when run: ODev.loop() */
 CUSTOM_CONNECTION_CLASS custom_connection_loop(DeviceConnection *conn){
 
-	if (EthernetClient newClient = server.available()) {
-
+	if (EthernetClient newClient = ethserver.available()) {
 		ethclient = newClient;
-
-//		// only if new client.
-//		if (!client || client != &newClient) {
-//
-//			client = &newClient;
-//		}
-
-//		if (newClient.connected()) {
-
-			Serial.println("Connected"); delay(200);
-
-//			conn->setStream((Stream*) &newClient);
-//			conn->checkDataAvalible();
-//		}
-
-	}else{
-//		conn->setStream(NULL);
 	}
 
 	return ethclient;
 
 }
-//
-//void ext_connection_readdata(EthernetClient& p_client){
-//	if(client){
-//		Serial.println("DB:ext_connection_readdata!"); delay(500);
-//		    boolean currentLineIsBlank = true;
-//		    while (client.connected()) {
-//		    	Serial.println("DB:ext_connection_conected !"); delay(500);
-//
-//		      if (client.available()) {
-//		        char c = client.read();
-//		        Serial.write(c);
-//		        // if you've gotten to the end of the line (received a newline
-//		        // character) and the line is blank, the http request has ended,
-//		        // so you can send a reply
-//		        if (c == '\n' && currentLineIsBlank) {
-//		          // send a standard http response header
-//		          client.println("HTTP/1.1 200 OK");
-//		          client.println("Content-Type: text/html");
-//		          client.println("Connection: close");  // the connection will be closed after completion of the response
-//		          client.println();
-//		          client.println("<!DOCTYPE HTML>");
-//		          client.println("<html>");
-//		          // output the value of each analog input pin
-//		          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-//		            int sensorReading = analogRead(analogChannel);
-//		            client.print("analog input ");
-//		            client.print(analogChannel);
-//		            client.print(" is ");
-//		            client.print(sensorReading);
-//		            client.println("<br />");
-//		          }
-//		          client.println("</html>");
-//		          break;
-//		        }
-//		        if (c == '\n') {
-//		          // you're starting a new line
-//		          currentLineIsBlank = true;
-//		        }
-//		        else if (c != '\r') {
-//		          // you've gotten a character on the current line
-//		          currentLineIsBlank = false;
-//		        }
-//		      }
-//		    }
-//
-//		    // give the web browser time to receive the data
-//			delay(1);
-//			// close the connection:
-//			client.stop();
-//	}
-//}
-
 
