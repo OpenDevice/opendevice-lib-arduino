06a0b0cbe6dad10ad7a941434a54cdc3b1127671
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2015-11-01 03:31:45 -0400

    #fixed conversion of device value (unsigned long)

--- src/DeviceConnection.cpp
+++ src/DeviceConnection.cpp
@@ -326,14 +326,14 @@ void DeviceConnection::send(double n){
 void DeviceConnection::send(Command cmd, bool complete){
 	if(!conn || processing) return;
 
-	long values[] = {cmd.type, cmd.id, cmd.deviceID, cmd.value};
+	unsigned long values[] = {cmd.type, cmd.id, cmd.deviceID, cmd.value};
 
 	conn->flush();
 
 	conn->write(START_BIT);
 	char vbuffer[3];
 	for (int i = 0; i < 4; ++i) {
-		ltoa(values[i], vbuffer, 10);
+		utoa(values[i], vbuffer, 10);
 		conn->write(vbuffer);
 		if(complete && i == 3){
 			conn->write(ACK_BIT);
