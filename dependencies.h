d6ee1547bc135a1f3c6e5dcdcae7d53f2f950f40
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2015-11-01 03:37:02 -0400

    New mechanism to manage dependencies

---
+++ dependencies.h
@@ -0,0 +1,25 @@
+/*
+ * ******************************************************************************
+ *  Copyright (c) 2013-2014 CriativaSoft (www.criativasoft.com.br)
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Ricardo JL Rufino - Initial API and Implementation
+ * *****************************************************************************
+ */
+
+#ifndef LIBRARIES_OPENDEVICE_DEPENDENCIES_H_
+#define LIBRARIES_OPENDEVICE_DEPENDENCIES_H_
+
+// *******************************************************************************************
+// Configure dependencies used in your project (Please comment unused dependencies)
+// *******************************************************************************************
+
+#include <MFRC522.h>
+
+
+
+#endif /* LIBRARIES_OPENDEVICE_SRC_DEPENDENCIES_H_ */
