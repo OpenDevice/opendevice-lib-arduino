8e1bdd214145da32e194bd0deafcda6c17154dba
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2015-11-01 03:33:31 -0400

    fixed #44 - RFID Native Integration

---
+++ src/devices/RFIDSensor.cpp
@@ -0,0 +1,56 @@
+/*
+ * ******************************************************************************
+ *  Copyright (c) 2013-2014 CriativaSoft (www.criativasoft.com.br)
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Ricardo JL Rufino - Initial API and Implementation
+ * *****************************************************************************
+ */
+
+#include "../dependencies.h"
+
+#if defined(MFRC522_h)
+
+#include "RFIDSensor.h"
+
+RFIDSensor::RFIDSensor(byte chipSelectPin, byte resetPin) :
+		Device(0, 0, Device::ANALOG, true), mfrc522(chipSelectPin, resetPin) {
+
+	SPI.begin();
+	mfrc522.PCD_Init();
+
+}
+
+RFIDSensor::~RFIDSensor() {
+
+}
+
+bool RFIDSensor::hasChanged(){
+	// Look for new cards
+	if (!mfrc522.PICC_IsNewCardPresent()) {
+		return false;
+	}
+
+	// Select one of the cards
+	if (!mfrc522.PICC_ReadCardSerial()) {
+		return false;
+	}
+
+	if (mfrc522.uid.size == 4) {
+
+		byte *raw = mfrc522.uid.uidByte;
+
+		currentValue = (unsigned long) (raw[3] << 24) | (raw[2] << 16) | (raw[1] << 8) | raw[0];
+		mfrc522.PICC_HaltA(); // mark as 'READ'
+		return true;
+	}
+
+	return false;
+
+}
+
+#endif
