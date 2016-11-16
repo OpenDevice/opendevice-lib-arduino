1e2d3e478fc04c65200cfdcdfabdcb8532568700
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2016-11-15 23:35:02 -0300

    Update examples

--- examples/BluetoothConnection/BluetoothConnection.ino
+++ examples/BluetoothConnection/BluetoothConnection.ino
@@ -1,17 +1,14 @@
-/*
- * *****************************************************************************
- * See tutorial: https://opendevice.atlassian.net/wiki/display/DOC/A.+First+Steps+with+OpenDevice
- * This example code is in the public domain.
- * *****************************************************************************
- */
- 
+//******************************************************************************
+// See tutorial: 
+// https://opendevice.atlassian.net/wiki/display/DOC/Getting+started
+//******************************************************************************
+
 #include <OpenDevice.h>
 
 void setup(){
-    ODev.enableDebug();
     ODev.addDevice("LED", 13, Device::DIGITAL); // ID:1
-    // ODev.begin(); // UNO
-    ODev.begin(Serial1, 9600); //-- for Leonardo, Teensy (Rx:0,Tx:1)
+    ODev.begin(); // UNO
+    //ODev.begin(Serial1, 9600); //-- for Leonardo, Teensy (Rx:0,Tx:1)
 }
 
 void loop(){
