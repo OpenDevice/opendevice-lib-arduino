3e91b45b9f691b6944072b2de5d4b5353391f959
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2018-07-08 18:49:20 -0300

    refractor logger with template params

--- src/utility/Logger.h
+++ src/utility/Logger.h
@@ -26,11 +26,11 @@
 #endif
 
 #if DEBUG || defined(DEBUG_ESP_PORT)
-#define LOG_DEBUG(title, val) Logger.debug(F(title), val)
-#define LOG_DEBUG_S(title) Logger.debug(F(title), NULL)
+#define LOG_DEBUG(...) Logger.debug(__VA_ARGS__)
+#define LOG_DEBUG_S(...) Logger.debug(__VA_ARGS__)
 #else
 #define LOG_DEBUG(...)
-#define LOG_DEBUG_S(title)
+#define LOG_DEBUG_S(...)
 #endif
 
 
@@ -38,7 +38,8 @@ namespace od {
 
 typedef struct{
 
-		void debug(const char title[], const char str[] = "", bool newLine = true){
+		template<typename T, typename M = char> void debug(const T title, const M str, bool newLine = true){
+
 			// if(Config.debugMode){
 				// if(Config.debugTarget == 1){
 //					deviceConnection->doStart();
@@ -49,7 +50,7 @@ typedef struct{
 					// #if(ENABLE_SERIAL)
 					Serial.print("DB:");
 					Serial.print(title);
-					if(str && strlen(str) > 0){
+					if(str){
 						Serial.print(" :: ");
 						if(newLine) Serial.println(str);
 						else Serial.print(str);
@@ -61,30 +62,37 @@ typedef struct{
 			// }
 		}
 
-	//#ifdef __FlashStringHelper
-		template <class T> void debug(const __FlashStringHelper* title, const T str, bool newLine = true){
-			Serial.print("DB:");
-			Serial.print(title);
-			if(str){
-				Serial.print(" :: ");
-				if(newLine) Serial.println(str);
-				else Serial.print(str);
-			}else{
-				Serial.println();
-			}
+		template<typename T> void debug(const T title, bool newLine = true){
+			char* messasge = nullptr;
+			debug(title,messasge,newLine);
 		}
+
+
+
+	//#ifdef __FlashStringHelper
+		// template <class T> void debug(const __FlashStringHelper* title, const T str, bool newLine = true){
+		// 	Serial.print("DB:");
+		// 	Serial.print(title);
+		// 	if(str){
+		// 		Serial.print(" :: ");
+		// 		if(newLine) Serial.println(str);
+		// 		else Serial.print(str);
+		// 	}else{
+		// 		Serial.println();
+		// 	}
+		// }
 	//#endif
 
 
-		void debug(const char title[], int val, bool newLine = true){
-			#if(DEBUG)
-			Serial.print("DB:");
-			Serial.print(title);
-			Serial.print(" :: ");
-			Serial.print(val, DEC);
-			Serial.println();
-			#endif
-		}
+		// void debug(const char title[], int val, bool newLine = true){
+		// 	#if(DEBUG)
+		// 	Serial.print("DB:");
+		// 	Serial.print(title);
+		// 	Serial.print(" :: ");
+		// 	Serial.print(val, DEC);
+		// 	Serial.println();
+		// 	#endif
+		// }
 
 	} LoggerClass;
 
